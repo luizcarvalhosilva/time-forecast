b90477927a04feaed943f7f719f8524c154c8a91
luizcarvalhosilva <156116231+luizcarvalhosilva@users.noreply.github.com>
2025-01-15 18:36:37 -0300

    feat: adicionar variável do useRef, input e botão para cidade
    - Criada a variável com useRef para capturar o valor da cidade.
    - Adicionados input e botão para entrada do valor da cidade.

--- time-forecast/src/App.jsx
+++ time-forecast/src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import axios from 'axios'
 import './App.css'
 
 function App() {
+
+  const cityInputRef = useRef();
 
   async function citySearch() {
     const city = cityInputRef.current.value;
     const apiKey = "ed3e7e048374f3bb1da9bc490dbf8e7e";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`;
     const gatheredData = await axios.get(url)
+  }
+
   return (
     <>
       <h1>teste</h1>
+      <input ref={cityInputRef} type="text" placeholder='digite a cidade'/>
+      <button onClick={citySearch}>Buscar</button>
     </>
   )
 }
