0883c231b609cac54475db2ff6db753cca062180
luizcarvalhosilva <156116231+luizcarvalhosilva@users.noreply.github.com>
2025-01-15 18:34:29 -0300

    chore: instalar Axios e configurar URL da API
    - Instalado o Axios para realizar requisições à API.
    - Criada variável para armazenar chave da API.
    - Ajustada a URL da API para incluir idioma em português e unidades métricas.

--- time-forecast/src/App.jsx
+++ time-forecast/src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
 
+    const apiKey = "ed3e7e048374f3bb1da9bc490dbf8e7e";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`;
+    const gatheredData = await axios.get(url)
   return (
     <>
       <h1>teste</h1>
