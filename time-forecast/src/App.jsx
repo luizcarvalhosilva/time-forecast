72913bb05ef8126f880fd82d395ccc161b13869c
luizcarvalhosilva <156116231+luizcarvalhosilva@users.noreply.github.com>
2025-01-15 18:35:48 -0300

    feat: criar função citySearch
    - Implementada a função citySearch para obter o valor do input da cidade e processá-lo.

--- time-forecast/src/App.jsx
+++ time-forecast/src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 
 function App() {
 
+  async function citySearch() {
+    const city = cityInputRef.current.value;
     const apiKey = "ed3e7e048374f3bb1da9bc490dbf8e7e";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`;
     const gatheredData = await axios.get(url)
